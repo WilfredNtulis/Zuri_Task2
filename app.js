b37a258215dd94e8e0b5931907e8f4bef4e31f4f
Wilfred <70230824+WilfredNtulis@users.noreply.github.com>
2021-08-20 19:51:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var sent = false;
 app.use('/public', express.static('public'));
 app.set('view engine', "ejs");
 app.get('/', function(req, res){
-    res.render('index', {sent: sent})
+    res.render('index', {sent:false})
 })
 app.post('/form', function(req, res){
     sent = true;
-    res.redirect('/');
+    res.render('index', {sent: true})
 })
 
 app.listen(port, () => {
